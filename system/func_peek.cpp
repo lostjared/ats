fc8b6dee358166e72b30f78e4218da9f915646c2
lostjared <jaredbruni@gmail.com>
2017-04-29 10:36:13 -0700

    updated peek/poke

--- system/func_peek.cpp
+++ system/func_peek.cpp
@@ -15,6 +15,6 @@ namespace token {
             return;
         }
         
-        std::cout << "[" << std::hex << std::uppercase << val << "] = " << std::hex << std::uppercase << static_cast<int>(code.peek(val)) << "\n";
+        std::cout << "[ $" << std::hex << std::uppercase << val << " ] = $" << std::hex << std::uppercase << static_cast<int>(code.peek(val)) << "\n";
     }
 }
