d142c51e57951846d201270f58060818ea99eccf
lostjared <jaredbruni@gmail.com>
2017-05-05 16:14:58 -0700

    added lookup to help command and how to use strings to help info

--- system/func_help.cpp
+++ system/func_help.cpp
@@ -27,7 +27,6 @@ namespace token {
     };
     
     void token_Help(const std::string &command, std::vector<lex::Token> &tokens) {
-        std::cout << "Command list:\n\nquit\ndescription: quit application\n\n";
         static std::vector<SortedCommand> names;
         if(names.size() == 0) {
             for(auto i = function_map.begin(); i != function_map.end(); ++i) {
@@ -35,8 +34,23 @@ namespace token {
             }
             std::sort(names.begin(), names.end());
         }
-        for(auto i = names.begin(); i != names.end(); ++i) {
-            std::cout << i->name  << "\n" "description: " <<  i->cmd.help_text << "\n\n";
+        if(tokens.size()==2) {
+            std::string value = tokens[1].getToken();
+            auto val = function_map.find(value);
+            if(val == function_map.end()) {
+                std::cerr << "Error: requires valid command to look up.\n";
+                return;
+            }
+            std::string textValue = val->second.help_text;
+            std::cout << "Description: " << textValue << "\n\n";
+        } else if(tokens.size()==1) {
+            std::cout << "Command list:\n\nquit\ndescription: quit application\n\n";
+        	for(auto i = names.begin(); i != names.end(); ++i) {
+            	std::cout << i->name  << "\n" "Description: " <<  i->cmd.help_text << "\n\n";
+        	}
+        } else {
+            std::cerr << "Error: requires zero or one argument of command to look up.\n";
+            return;
         }
     }
 }
