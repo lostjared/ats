40ab40f73da4d3f83932edf475fdd926b880fb29
lostjared <jaredbruni@gmail.com>
2017-05-10 12:16:35 -0700

    now you can insert symbols from symbol table into memory as a list of variables/values

--- system/func_insert.cpp
+++ system/func_insert.cpp
@@ -12,31 +12,37 @@ namespace token {
             uint32_t start_address = icode::toHex(tokens[1].getToken());
             unsigned int i = 0;
             for(i = 2; i < tokens.size(); ++i) {
-                if(tokens[i].getTokenType() != lex::TOKEN_HEX) {
+                if(tokens[i].getTokenType() == lex::TOKEN_CHAR) {
+                    if(!code.symbols.exisits(tokens[i].getToken())) {
+                        std::cerr << "Error: Symbol: " << tokens[i].getToken() << " not found in table..\n";
+                        return;
+                    }
+                    
+                }
+                else if(tokens[i].getTokenType() != lex::TOKEN_HEX) {
                     std::cerr << "Error: list of values must all be in hex.\n";
                     return;
                 }
             }
-            
             for(i = 2; i < tokens.size(); ++i) {
                 uint32_t address = start_address+(i-2);
-                
                 if(address > 0xFFFF) {
                     std::cerr << "Error: memory out of bounds..\n";
                     return;
                 }
+                uint8_t hexValue = 0;
+                if(tokens[i].getTokenType() == lex::TOKEN_CHAR)
+                    hexValue = static_cast<uint8_t>(code.symbols[tokens[i].getToken()].get_double());
+                else
+                    hexValue = static_cast<uint8_t>(icode::toHex(tokens[i].getToken()));
                 
-                uint32_t hexValue = icode::toHex(tokens[i].getToken());
+                std::cout << "[ $" << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << address << " ] = $" << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << static_cast<uint32_t>(hexValue) << "\n";
                 
-                std::cout << "[ " << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << address << " ] = " << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << hexValue << "\n";
-                
-                code.poke(static_cast<uint16_t>(address), static_cast<uint8_t>(hexValue));
+                code.poke(static_cast<uint16_t>(address),hexValue);
             }
-            
         } else {
             std::cerr << "Error: requires starting address followed by values\n";
             return;
         }
     }
-    
 }
